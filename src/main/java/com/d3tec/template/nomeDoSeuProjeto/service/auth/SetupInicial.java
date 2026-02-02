7f2097c699cc4b8b8f067beb665e132c4025edc4
Miguel Silvano <miguelsilvano.20@hotmail.com>
2026-02-01 22:45:45 -0300

    fix - so faz o log do link do swagger quando o profile ativo for dev

--- src/main/java/com/d3tec/template/nomeDoSeuProjeto/service/auth/SetupInicial.java
+++ src/main/java/com/d3tec/template/nomeDoSeuProjeto/service/auth/SetupInicial.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -66,6 +68,9 @@ public class SetupInicial implements ApplicationRunner {
 
     @EventListener(ApplicationReadyEvent.class)
     public void logSwaggerUrl() {
+        if (!environment.acceptsProfiles(Profiles.of("dev")))
+            return;
+
         String port = environment.getProperty("local.server.port", environment.getProperty("server.port", "8080"));
 
         String base = "http://localhost:" + port + (contextPath == null ? "" : contextPath);
